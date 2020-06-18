1ab11283ecf5762050554dc3a0e577fd68ab76cb
Lucas William <lucaswilliameufrasio@gmail.com>
2020-06-18 19:22:07 -0300

    ensure that LoginRouter returns 400 status if no email is provided

---
+++ src/presentation/routers/login-router.spec.js
@@ -0,0 +1,24 @@
+class LoginRouter {
+  route(httpRequest) {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Login Router', () => {
+  test('It should return 400 status if no email is provided', () => {
+    // sut = system under test
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: 'any'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
