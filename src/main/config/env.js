fa6501b5b91d9427a24e57e45be0e56ea5586bad
Lucas William <lucaswilliameufrasio@gmail.com>
2020-06-29 07:03:31 -0300

    refactor: move loginRouter composition to its own file

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/clean-node-api'
+    mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/clean-node-api',
+    tokenSecret: process.env.TOKEN_SECRET || 'secret'
 }
